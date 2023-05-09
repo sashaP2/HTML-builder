2bcfe448558ff9a87e45b63736c81656d5ad65ac
Sasha Petushkova <sashpet_2000@mail.ru>
2023-05-09 09:50:30 +0300

    feat: add code for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const path = require('path');
 
+const pathMainFolder = path.join(__dirname, 'files');
+const pathNewFolder = path.join(__dirname, 'files-copy');
+
+fs.mkdir(pathNewFolder, { recursive: true }, (err) => {
+    if (err) console.log(err);
+});
+
+const obj = fs.promises.readdir(pathMainFolder);
+
+obj.then(files => files.forEach(file => {
+
+  const pathMainFile = path.resolve(pathMainFolder, file);
+  const pathNewFile = path.resolve(pathNewFolder, file);
+
+  fs.promises.copyFile(pathMainFile, pathNewFile);
+})
+);
