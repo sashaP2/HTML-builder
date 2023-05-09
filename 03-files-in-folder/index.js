15a625858b2797426e620fef2231623d3e8bca0a
Sasha Petushkova <sashpet_2000@mail.ru>
2023-05-09 06:47:41 +0300

    feat: add code for 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(pathFolder, {withFileTypes: true}, (err, files) => {
+  if (err) console.log(err);
+
+  files.forEach((key) => {
+    if (key.isFile()) {
+
+      const pathFile = path.join(pathFolder, key.name);
+
+      fs.stat(pathFile, (err, files) => {
+      if (err) console.log(err);
+
+      const name = key.name.split('.')[0];
+      const ext = path.extname(pathFile).slice(1);
+      const size = files.size;
+      const sizeKb = size/1024;
+
+        console.log(`${name} -- ${ext} -- ${sizeKb}kb`);
+      });
+    }
+  });
+});
